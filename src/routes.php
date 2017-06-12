6494cb88829743e00311f88f1e78b6673d682449
Tung Mang Vien <tungmv7@gmail.com>
2017-06-12 15:12:08 +0700

    init source

---
+++ src/routes.php
@@ -0,0 +1,65 @@
+<?php
+// Routes
+
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+
+	$this->logger->info("Slim-Skeleton '/' route");
+
+	$result = [];
+
+	$params = $request->getQueryParams();
+
+	$response = $response->withHeader('Content-type', 'application/json');
+
+	if (isset($params['lat']) && isset($params['lng'])) {
+		$center_lat = $params['lat'];
+		$center_lng = $params['lng'];
+		$radius = isset($params['radius']) ? $params['radius'] : 25;
+		
+		$db = new PDO("mysql:host=localhost;dbname=test", 'root', '123@cms');
+		$query = sprintf("SELECT *, ( 3959 * acos( cos( radians('%s') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians('%s') ) + sin( radians('%s') ) * sin( radians( lat ) ) ) ) AS distance FROM mytable HAVING distance < '%s' ORDER BY distance LIMIT 0 , 5",
+			  ($center_lat),
+			  ($center_lng),
+			  ($center_lat),
+			  ($radius));
+		$t = $db->query($query);
+
+		foreach ($t->fetchAll() as $key => $value) {
+			$result[] = [
+				'Company' => $value['Company'],
+				'Address' => $value['Address'],
+				'Suburb' => $value['Suburb'],
+				'Locality' => $value['Locality'],
+				'PostCode' => $value['PostCode'],
+				'Email' => $value['Email'],
+				'Phone' => $value['Phone'],
+				'Lat' => $value['Lat'],
+				'Lng' => $value['Lng']
+			];
+		}
+	}
+
+	return $response->withJson($result);
+
+	// $db = new PDO("sqlite:..//db.sqlite");
+	// $sql = "Select * from mytable";
+
+	// $result = [];
+	// foreach ($db->query($sql) as $key => $value) {
+	// 	$result[] = [
+	// 		'Company' => $value['Company'],
+	// 		'Address' => $value['Address'],
+	// 		'Suburb' => $value['Suburb'],
+	// 		'Locality' => $value['Locality'],
+	// 		'PostCode' => $value['PostCode'],
+	// 		'Email' => $value['Email'],
+	// 		'Phone' => $value['Phone'],
+	// 		'Lat' => $value['Lat'],
+	// 		'Lng' => $value['Lng']
+	// 	];
+	// }
+	// $response->withJson($result);
+
+	// return $response;
+});
